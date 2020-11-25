b7a42b26a9100af4bac5ed0fa260026d07f0566b
valerioh1005 <61766607+valerioh1005@users.noreply.github.com>
2020-11-25 07:06:31 -0500

    final JS code

--- js/app.js
+++ js/app.js
@@ -23,8 +23,71 @@ overlay.addEventListener('click', () => {
 });
 
 // Randon phrase array function
-const getRandomPhraseArray = () => {
-    let randomNumber = Math.floor(Math.random() * (phrases.length) );
+const getRandomPhraseArray = (arr) => {
+    let randomNumber = Math.floor(Math.random() * phrases.length );
+    return phrases[randomNumber];
 };
 
 getRandomPhraseArray(phrases);
+
+
+function addPhraseToDisplay(arr) {
+    for ( let i = 0; i < arr.length; i++) {
+      let liList = document.createElement('li');
+      let ulList = document.querySelector('#phrase ul');
+      liList.textContent = arr[i];
+      ulList.appendChild(liList);
+  
+      if (liList.textContent === ' ') {
+        liList.className = 'space';
+      } else {
+        liList.className = 'letter';
+      }
+    }
+  };
+  
+  const phraseArray = getRandomPhraseArray(phrases);
+  addPhraseToDisplay(phraseArray)
+  
+  function checkLetter(button) {
+    let checkLetter = document.querySelectorAll('.letter');
+    let match = null;
+  
+    for ( let i = 0; i < checkLetter.length; i++) {
+      if (checkLetter[i].textContent === button.textContent) {
+        checkLetter[i].classList.add('show');
+        match = true;
+      }
+    }
+    return match;
+  };
+  
+  qwerty.addEventListener('click', (e) => {
+    if( e.target.tagName === 'BUTTON' ) {
+      e.target.className = 'chosen';
+    } if ( e.target.className === 'chosen' ) {
+      e.target.disabled = 'true';
+      let letterFound = checkLetter(e.target);
+      if ( letterFound === null ) {
+        missed += 1;
+        lostHeart[missed - 1].src = "images/lostHeart.png";
+      }
+    }
+    checkWin();
+  });
+  
+  function checkWin () {
+    const letter = document.querySelectorAll('.letter');
+    const show = document.querySelectorAll('.show');
+  
+    if ( letter.length === show.length ) {
+      overlay.className = 'win';
+      document.querySelector('h2').innerHTML = 'Congrats You Win!';
+      overlay.style.display = 'flex';
+    } else if ( missed > 4 ) {
+      overlay.className = 'lose';
+      document.querySelector('h2').innerHTML = "You lost! Try again";
+      overlay.style.display = 'flex';
+    }
+  };
+  
